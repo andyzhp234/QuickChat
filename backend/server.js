a813de90aad6d5afd2cc65c27659c0fc3ce08f07
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-14 04:12:31 -0800

    chore(CORS): update policy

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173"],
+    origin: ["http://127.0.0.1:5173", "https://quickchat-app.netlify.app"],
     credentials: true,
   })
 );
--- backend/socketServer.js
+++ backend/socketServer.js
@@ -21,7 +21,7 @@ let io;
 const registerSocketServer = (server, sessionMiddleware) => {
   io = new Server(server, {
     cors: {
-      origin: "http://127.0.0.1:5173",
+      origin: ["http://127.0.0.1:5173", "https://quickchat-app.netlify.app"],
       credentials: true,
     },
   });
