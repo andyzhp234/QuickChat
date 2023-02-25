7a8fb15aee0016e2df77114cb61b77edc9c9d726
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-25 15:42:35 -0800

    updated README & Privacy Policy

--- frontend/src/pages/PrivacyPolicy/index.jsx
+++ frontend/src/pages/PrivacyPolicy/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function PrivacyPolicy() {
-  return <div>index</div>;
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <h1>Last updated: February 25, 2023</h1>
+    </div>
+  );
 }
--- frontend/src/pages/TermsOfServices/index.jsx
+++ frontend/src/pages/TermsOfServices/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function TermsOfServices() {
-  return <div>index</div>;
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+    </div>
+  );
 }
