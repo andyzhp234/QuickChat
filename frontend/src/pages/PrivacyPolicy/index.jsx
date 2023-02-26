f329fef05e46b5db38f66e3824abbf03917179bf
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-25 20:00:28 -0800

    feat(registration): add information

--- frontend/src/pages/PrivacyPolicy/index.jsx
+++ frontend/src/pages/PrivacyPolicy/index.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 
 export default function PrivacyPolicy() {
-  return (
-    <div>
-      <h1>Privacy Policy</h1>
-      <h1>Last updated: February 25, 2023</h1>
-    </div>
-  );
+  return <div></div>;
 }
