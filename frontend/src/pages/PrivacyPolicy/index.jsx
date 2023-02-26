e188f9aa58747db98d47e8e7b3b5e904fbf91c7a
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-25 23:59:24 -0800

    finalized Privacy Policy

--- frontend/src/pages/PrivacyPolicy/index.jsx
+++ frontend/src/pages/PrivacyPolicy/index.jsx
@@ -1,5 +1,173 @@
 import React from "react";
 
 export default function PrivacyPolicy() {
-  return <div></div>;
+  return (
+    <div className="py-10 px-20">
+      <h1 className="py-5 text-6xl">Privacy Policy</h1>
+      <h2 className="py-1">Last updated: February 25, 2023</h2>
+      <h2 className="py-1">
+        At Quick Chat, accessible at quick-chat.app, one of our main priorities
+        is the privacy of our visitors. This Privacy Policy document contains
+        types of information that is collected and recorded by Quick Chat and
+        how we use it. If you have additional questions or require more
+        information about our Privacy Policy, do not hesitate to contact us
+        through email at haopeng.zeng234@gmail.com. We promise we never sell
+        your data.
+      </h2>
+      <h1 className="py-2 text-4xl font-semibold">General Overview</h1>
+      <h2 className="py-1">
+        By accepting the following terms, you consent to our use and disclosure
+        of your personal information in accordance with this Privacy Policy.
+        This Privacy Policy is incorporated into and subject to the guidelines
+        of our Terms of Use. This Privacy Policy is effective upon registration
+        for new users, and is otherwise effective on February 25, 2023 for all
+        users.
+      </h2>
+      <h1 className="py-2 text-4xl font-semibold">
+        Information We Collect and Usage
+      </h1>
+      <h2 className="py-1">
+        When you sign up for a Quick Chat Account, we ask for identifying
+        information such as your name, username, email address, and password to
+        provide you with a safe, customized, and efficient experience. "Cookies"
+        are pieces of information that are passed to your browser and stored on
+        your computer's hard drive. We use cookies on our website to enhance
+        your experience and for authentication purposes. The cookie we use is an
+        httponly cookie, which is a functional cookie that is used solely for
+        authentication purposes. This cookie is not used to track your activity
+        on our website or collect any personal information about you. It is also
+        not accessible to any third-party websites. You can adjust cookie
+        retention settings and accept or block individual cookies in your
+        browser settings, although our apps won't work and other aspects of our
+        service may not function properly if you turn cookies off. By using our
+        website, you consent to the use of this cookie for authentication
+        purposes. Accounts created on our website are subject to periodic
+        deletion. While we make every effort to delete accounts in a timely
+        manner, the exact timing of these deletions may vary and is not
+        guaranteed. By creating an account on our website, you acknowledge and
+        agree that your account may be deleted at any time without prior notice.
+        We'll never sell your personal information to third parties, and we
+        won't use your name and email in marketing statements without your
+        permission either.
+      </h2>
+      <h1 className="py-2 text-4xl font-semibold">Information Security</h1>
+      <h2 className="py-1">
+        We try our best to help protect the confidentiality and integrity of
+        personal information and to reduce the risk of loss, misuse,
+        unauthorized access, disclosure, or modification of personal
+        information. You should be aware that any information transmitted
+        electronically via the World Wide Web might not be 100% secure. Quick
+        Chat, and its developer Haopeng Zeng assumes no liability for the loss
+        of any information that you transmit to us via the World Wide Web. Most
+        data are not encrypted while they live in our database, but your account
+        passwords are encrypted before they are stored in our database.
+      </h2>
+      <h2 className="py-1">
+        All user passwords are hashed using a strong cryptographic algorithm,
+        and only the site owner have the access to data informations.
+      </h2>
+      <h2 className="py-1">
+        Although we take reasonable measures to protect your personal
+        information, please be aware that no security measures are foolproof,
+        and there is always a risk of data breaches. In the event of a data
+        breach, we may not be able to notify you in a timely manner. By using
+        our website or services, you acknowledge and accept these risks and
+        agree to hold us harmless in the event of a breach. We encourage you to
+        take steps to protect your own personal information, such as using
+        strong passwords, being cautious about sharing personal information
+        online, and monitoring your accounts for suspicious activity.
+      </h2>
+      <h1 className="py-2 text-4xl font-semibold">Service Providers</h1>
+      <h2 className="py-1">
+        We may use third-party service providers to help us operate our chat
+        app, including but not limited to hosting our front-end code on Netlify
+        and our back-end infrastructure on Railways.app. These service providers
+        may have access to your Personal Data only to the extent necessary to
+        perform their functions and are obligated not to disclose or use it for
+        any other purpose. Please note that we are not responsible or liable for
+        the actions or omissions of these third-party service providers,
+        including but not limited to the loss of any information they may cause.
+      </h2>
+      <h1 className="py-2 text-4xl font-semibold">
+        Access, Modify or Delete Your Personal Information
+      </h1>
+      <h2 className="py-1">
+        If you request that we cancel your account, you will not be able to use
+        any of the services on our site unless you re-register. We will retain
+        your information for as long as your account is active or as needed to
+        provide you services. If you wish to cancel / delete your account
+        contact us. We will retain and use your information as necessary to
+        comply with our legal obligations, resolve disputes, and enforce our
+        agreements.
+      </h2>
+      <h1 className="py-2 text-4xl font-semibold">
+        Legal Basis For Processing Personal Data Under The General Data
+        Protection Regulation (GDPR)
+      </h1>
+      <h2 className="py-1">
+        If you are from the European Economic Area (EEA), Quick Chat legal basis
+        for collecting and using the personal information described in this
+        Privacy Policy depends on the Personal Data we collect and the specific
+        context in which we collect it. Quick Chat may process your Personal
+        Data because: You have given us permission to do so The processing is in
+        our legitimate interests and it is not overridden by your rights To
+        comply with the law
+      </h2>
+      <h1 className="py-2 text-4xl font-semibold">
+        Your Data Protection Rights Under The General Data Protection Regulation
+        (GDPR)
+      </h1>
+      <h2 className="py-1">
+        If you are a resident of the European Economic Area (EEA), you have
+        certain data protection rights. Quick Chat aims to take reasonable steps
+        to allow you to correct, amend, delete or limit the use of your Personal
+        Data. If you wish to be informed about what Personal Data we hold about
+        you and if you want it to be removed from our systems, please contact
+        us. In certain circumstances, you have the following data protection
+        rights: The right to access, update or delete the information we have on
+        you: Whenever made possible, you can access, update or request deletion
+        of your Personal Data directly within your account settings section. If
+        you are unable to perform these actions yourself, please contact us to
+        assist you. The right of rectification: You have the right to have your
+        information rectified if that information is inaccurate or incomplete.
+        The right to object: You have the right to object to our processing of
+        your Personal Data. The right of restriction: You have the right to
+        request that we restrict the processing of your personal information.
+        The right to data portability: You have the right to be provided with a
+        copy of the information we have on you in a structured, machine-readable
+        and commonly used format. The right to withdraw consent: You also have
+        the right to withdraw your consent at any time where Quick Chat relied
+        on your consent to process your personal information. Please note that
+        we may ask you to verify your identity before responding to such
+        requests. You have the right to complain to a Data Protection Authority
+        about our collection and use of your Personal Data. For more
+        information, please contact your local data protection authority in the
+        European Economic Area (EEA).
+      </h2>
+      <h1 className="py-2 text-4xl font-semibold">Children Informations</h1>
+      <h2 className="py-1">
+        Another part of our priority is adding protection for children while
+        using the internet. We encourage parents and guardians to observe,
+        participate in, and/or monitor and guide their online activity. Website
+        Name does not knowingly collect any Personal Identifiable Information
+        from children under the age of 13. If you think that your child provided
+        this kind of information on our website, we strongly encourage you to
+        contact us immediately and we will do our best efforts to promptly
+        remove such information from our records. The Website is not intended
+        for children under the age of 13. We do not knowingly collect personally
+        identifiable information via the Website from visitors in this age
+        group.
+      </h2>
+      <h1 className="py-2 text-4xl font-semibold">Contact Information</h1>
+      <h2 className="py-1">
+        Have any questions, comments, or concerns about this privacy policy,
+        your data, or your rights with respect to your information? Please get
+        in touch by emailing us at haopeng.zeng234@gmail.com and we'll be happy
+        to try to answer them!
+      </h2>
+      <h1 className="mt-10">
+        Adapted from the Basecamp open-source policies / CC BY 4.0
+      </h1>
+    </div>
+  );
 }
