6f5c07c2366503220b5bfa5842ccb67eeadde0cf
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-27 05:39:11 -0800

    feat: update join group functionalities

--- backend/socketHandler/groups/socketJoinGroup.js
+++ backend/socketHandler/groups/socketJoinGroup.js
@@ -6,7 +6,6 @@ import { sendSocketMessageToUser } from "../socketUtils.js";
 const socketJoinGroup = async (socket, data, callback) => {
   const conversationID = data.groupId;
   const userId = socket.request.session.userId;
-
   try {
     const chatRoom = await Conversation.findOne({
       where: { id: conversationID },
@@ -21,8 +20,10 @@ const socketJoinGroup = async (socket, data, callback) => {
 
     // check if we are already the participant
     const alreadyParticipant = await ConversationParticipant.findOne({
-      conversationID: conversationID,
-      participantId: userId,
+      where: {
+        conversationId: conversationID,
+        participantId: userId,
+      },
     });
     if (alreadyParticipant) {
       callback({
@@ -31,15 +32,12 @@ const socketJoinGroup = async (socket, data, callback) => {
       });
       return;
     }
-
     // otherwise we can join the conversation
     await ConversationParticipant.create({
       conversationId: conversationID,
       participantId: userId,
     });
-
     await sendSocketMessageToUser(userId, "new-groups-chatroom", chatRoom);
-
     callback({
       message: `Successfully Joined Group Chatroom!`,
     });
